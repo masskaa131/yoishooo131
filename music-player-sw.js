31c77f7fca251059fbc24181ed583cf7984e7b57
masskaa131 <pskento0131@gmail.com>
2026-09-06 13:18:47 +0900

    Add files via upload

---
+++ music-player-sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'local-music-player-v1';
+const APP_SHELL = ['./', './index%20(6).html'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => {
+    const network = fetch(event.request).then(response => {
+      if (response && response.ok) caches.open(CACHE_NAME).then(cache => cache.put(event.request, response.clone()));
+      return response;
+    });
+    return cached || network;
+  }));
+});
